ecd24b1a909e9cae0e0a9dd12dab3f56dd2edd3d
Suman Shrestha <sumanstha0614@gmail.com>
2026-06-02 23:28:33 +0545

    02(feat) :: Component Architecture

--- packages/cli/src/index.tsx
+++ packages/cli/src/index.tsx
@@ -1,11 +1,21 @@
-import { createCliRenderer, TextAttributes } from "@opentui/core";
+import { createCliRenderer } from "@opentui/core";
 import { createRoot } from "@opentui/react";
+import Header from "./components/header";
+import { InputBar } from "./components/input-bar";
 
 function App() {
   return (
-    <box alignItems="center" justifyContent="center" flexGrow={1}>
-      <box justifyContent="center" alignItems="flex-end">
-        <textarea placeholder="Enter your text here..." focused/>
+    <box
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      width="100%"
+      height="100%"
+      gap={2}
+    >
+      <Header />
+      <box width="100%" paddingX={2}>
+        <InputBar onSubmit={() => {}} />
       </box>
     </box>
   );
